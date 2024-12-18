7abf02034c3d195ab88e003666e0bcf916d9cfd4
Ayush <akagarwal.111a@gmail.com>
2024-12-18 13:24:14 +0530

    Added Cookie-Cutter Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project we predict the quality of water.',
+    author='Ayush Kumar',
+    license='MIT',
+)
