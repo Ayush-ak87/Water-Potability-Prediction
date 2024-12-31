b31c44e2bb73083bc63488873a2c6e4652449623
Ayush <akagarwal.111a@gmail.com>
2024-12-31 15:31:01 +0530

    changes

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -19,6 +19,8 @@ dagshub_token = os.getenv("DAGSHUB_TOKEN")
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
+print("DAGSHUB_TOKEN loaded:", dagshub_token)  # Debug print
+
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
